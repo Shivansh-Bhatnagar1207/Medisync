4de142aed92a79fb1d9887775725cac8cc75c1e2
Shivansh-Bhatnagar1207 <Shivanshb55@gmail.com>
2024-12-10 15:16:20 +0530

    login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,90 @@
+"use client";
+import { useWixClient } from "@/hooks/WixClient";
+import React, { useState } from "react";
+
+export default function Login() {
+  const [mode, setMode] = useState("LOGIN");
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailCode, setEmailCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+
+  const formTitle =
+    mode === "LOGIN"
+      ? "Log in"
+      : mode === "REGISTER"
+      ? "Register"
+      : mode === "RESET_PASSWORD"
+      ? "Reset Your Password"
+      : "Verify Your Email";
+
+  const buttonTitle =
+    mode === "LOGIN"
+      ? "Login"
+      : mode === "REGISTER"
+      ? "Register"
+      : mode === "RESET_PASSWORD"
+      ? "Reset"
+      : "Verify";
+
+  const wixClient = useWixClient();
+
+  return (
+    <div className="form-container">
+      <form>
+        <h1>{formTitle}</h1>
+        {mode === "REGISTER" ? (
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter Username"
+            required
+          />
+        ) : null}
+        {mode !== "EMAIL_VERIFICATION" ? (
+          <input name="email" type="email" placeholder="xyz@abc.com" required />
+        ) : (
+          <input name="emailCode" type="text" placeholder="CODE" required />
+        )}
+        {mode === "LOGIN" || mode === "REGISTER" ? (
+          <input
+            name="Password"
+            id="Password"
+            placeholder="Password"
+            type="password"
+            required
+          />
+        ) : null}
+        <button >
+         {buttonTitle}
+        </button>
+        {error && <div>{error}</div>}
+        {message && <div>{message}</div>}
+
+        {mode === "LOGIN" && (
+          <div
+            onClick={() => {
+              setMode("REGISTRATION");
+            }}
+          >
+            {"Don't"} have an account?
+          </div>
+        )}
+        {mode === "REGISTRATION" && (
+          <div
+            onClick={() => {
+              setMode("LOGIN");
+            }}
+          >
+            {"Don't"} have an account?
+          </div>
+        )}
+      </form>
+    </div>
+  );
+}
